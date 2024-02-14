9c7c80246d534a239e91357bb97203be367a64ea
Samuel Masih <samuelmasih.sls777@gmail.com>
2024-02-14 18:44:41 +0530

    cors

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const connectDB = require("./config/dbconfig")
+const cors = require('cors');
 const app = express();
+
 const hotelRouter = require('./routes/hotel.router')
 const categoryRouter = require('./routes/category.router');
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
@@ -11,6 +13,8 @@ const singleHotelRouter = require('./routes/singlehotel.router');
 const authRouter = require('./routes/auth.router')
 const wishlistRoute = require('./routes/wishlist.router');
 
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.json());
@@ -21,6 +25,7 @@ const PORT = 3500;
 app.get('/',(req,res)=>{
     res.send("hellow Samuel");
 })
+
 app.use("/api/hoteldata",hotelDataAddedToDBRouter);
 app.use("/api/categorydata",categoryDataAddedToDBRouter);
 app.use("/api/hotels",hotelRouter);
